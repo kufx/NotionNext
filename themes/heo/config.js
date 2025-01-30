b9f74cc02102ce40f94e421f428ca78e929b7289
kufx <144138627+kufx@users.noreply.github.com>
2025-01-31 02:24:35 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,9 +28,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '网站推荐', url: '/category/website' },
-  HEO_HERO_CATEGORY_2: { title: '软件推荐', url: '/category/app' },
-  HEO_HERO_CATEGORY_3: { title: '每日分享', url: '/category/share' },
+  HEO_HERO_CATEGORY_1: { title: '网站推荐', url: '/category/网站推荐' },
+  HEO_HERO_CATEGORY_2: { title: '软件推荐', url: '/category/软件推荐' },
+  HEO_HERO_CATEGORY_3: { title: '每日分享', url: '/category/每日分享' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '分享',
